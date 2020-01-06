8722bd863ea70c1c88bca947f260fa4b2887d2b8
Jon Harper <jon.harper87@gmail.com>
2020-01-06 18:01:38 +0100

    only arithmetic

--- app2/src/test/java/com/mycompany/app/AppTest.java
+++ app2/src/test/java/com/mycompany/app/AppTest.java
@@ -15,9 +15,11 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        int k=0;
+        long k=0;
         for (int i=1; i<2000000000; i++) {
-            k=k+(1%i);
+            for (int j=1; j<5; j++) {
+                k=k+i+j;
+            }
         }
         System.out.println(k);
     }
